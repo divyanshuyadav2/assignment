8132a779b524f2fb7402d4af32b6a6017f5003fd
Divyanshu Yadav <139464561+divyanshuyadav2@users.noreply.github.com>
2023-07-17 11:07:36 +0530

    updatedcode

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  var SectionView = Backbone.View.extend({
+    el: '.wrap-2',
+    events: {
+      'click .section-link': 'navigateToSection'
+    },
+    initialize: function() {
+      this.render();
+    },
+    render: function() {
+      this.$('.section').eq(0).addClass('active-section');
+    },
+    navigateToSection: function(e) {
+      e.preventDefault();
+      var sectionId = $(e.target).data('section-id');
+      var targetSection = $('#' + sectionId);
+      var targetPosition = targetSection.offset().top;
+
+      $('html, body').animate({
+        scrollTop: targetPosition
+      }, 800);
+
+      $('.section').removeClass('active-section');
+      targetSection.addClass('active-section');
+    }
+  });
+
+  var sectionView = new SectionView();
+});
